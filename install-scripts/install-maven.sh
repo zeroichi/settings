189a31c0db95e9d823c47fb6099dda3be964bb4c
zeroichi <zeroichi@users.noreply.github.com>
2022-07-03 08:27:03 +0000

    add: check sha512 hash in maven install script

--- install-scripts/install-maven.sh
+++ install-scripts/install-maven.sh
@@ -3,6 +3,8 @@ set -e
 
 VER=3.8.6
 URL=https://ftp.jaist.ac.jp/pub/apache/maven/maven-3/$VER/binaries/apache-maven-$VER-bin.tar.gz
+# note: paste hash from https://downloads.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz.sha512
+HASH=f790857f3b1f90ae8d16281f902c689e4f136ebe584aba45e4b1fa66c80cba826d3e0e52fdd04ed44b4c66f6d3fe3584a057c26dfcac544a60b301e6d0f91c26
 PKG_FILE=$(basename "$URL")
 INST_DIR=/opt/mvn
 SLINK_DIR=/usr/local/bin
@@ -11,6 +13,13 @@ echo "Downloading package from $URL ..."
 cd /tmp
 if [ ! -f "$PKG_FILE" -o "$1" = "-f" ]; then
     curl -L "$URL" -o "$PKG_FILE"
+    CALC_HASH=$(sha512sum "$PKG_FILE" | awk '{print $1}')
+    if [ $HASH != "$CALC_HASH" ]; then
+        echo "ERROR: sha512 hashes does not match"
+        echo "  expected:   $HASH"
+        echo "  downloaded: $CALC_HASH"
+        exit 1
+    fi
 fi
 echo
 
